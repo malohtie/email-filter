7ec5736c8f37f52f3d4e11c0a1cafc267cdbd5f0
med amine <mohamed.amine.elattabi@gmail.com>
2019-09-22 04:05:45 +0100

    improves

--- filter.py
+++ filter.py
@@ -12,6 +12,7 @@ domains = inputdomain.split(",")
 
 found = 0
 line = 0
+valid = []
 date = datetime.now().strftime("%d_%m_%Y_%H_%M_%S");
 filename = "./result_filter_"+date+".txt"
 
@@ -23,18 +24,32 @@ if my_file.is_file():
 			if email:
 				line +=1
 				domain = email[0].strip().lower().split('@');
-				for em in domains:
-					if domain[1].find(em) != -1:
-						found += 1
-						#print("EMAIL FOUND : "+email[0])
-						newfile = open(filename, "a")
-						newfile.write(email[0].strip().lower()+"\n")
-						newfile.close()
-						if(found % 100 == 0):
-							print("FOUND "+str(found)+" EMAILS -- CURRENT LINE "+str(line))
-						break
+				if len(domain) == 2:
+					for em in domains:
+						if domain[1].find(em) != -1:
+							found += 1
+							#print("EMAIL FOUND : "+email[0])
+							valid.append(email[0].strip().lower())
+							if(found % 100 == 0):
+								print("FOUND "+str(found)+" EMAILS -- CURRENT LINE "+str(line))
+							break
 	csvs.close()
 else:
 	print("FILE NOT FOUND")
 
-print('ENDED FOUND '+str(found))
+
+print("WRITING RESULT BE PATIENT :)")
+
+def is_valid_email(email):
+	if len(email) > 7:
+		return bool(re.search("^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$", email))
+
+validemail = 0
+with open(filename, 'w+', encoding='utf-8') as txt:
+	for data in valid:
+		if(is_valid_email(data)):
+			validemail += 1
+			txt.write(data+"\n")
+
+
+print('ENDED FOUND :'+str(found)+' VALID EMAILS :'+str(validemail))
